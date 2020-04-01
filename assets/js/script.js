ff601815b2d3f97b6892778e86eb11951c0801e6
Andy Osborne <ajaobsorne@googlemail.com>
2020-04-01 12:39:45 +0100

    Add responsive design to tutorial modal and allow to be launched by two different buttons

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+   var modal = document.getElementById("tutorial");
+   var btns = document.querySelectorAll(".tutorial-btn");
+   var close = document.getElementsByClassName("modal-close")[0];
+
+
+   [].forEach.call(btns, function(el) {
+    el.onclick = function() {
+        modal.style.display="block";
+    }
+})
+
+   close.onclick = function() {
+       modal.style.display = "none";
+   }
+
+   window.onclick = function(event) {
+       if (event.target == modal) {
+           modal.style.display = "none";
+       }
+   }
+
+
+
+
+});
