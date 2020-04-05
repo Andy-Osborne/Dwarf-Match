40580ddfc78eae1efe0f608551c21ac1285ac84f
Andy-Osborne <ajaosborne@googlemail.com>
2020-04-05 10:03:44 +0100

    Remove duplicate CSS classes and add function to record user level choice for game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,36 @@
-$(document).ready(function() {
-    
-    $(".tutorial-btn").click(function() {
-        $("#tutorial").addClass("display-block");
-    $(".modal-close").click(function() {
-        $(".modal").removeClass("display-block").addClass("display-none");
-    })
-})
+$(document).ready(function () {
 
-$(".play-btn").click(function() {
-    $("#levelSelect").addClass("display-block");
-    $(".modal-close").click(function() {
-        $(".modal").removeClass("display-block").addClass("display-none");
-    })
-})
+    /** 
+    * Modal Button controls works by adding display: block class when the corresponding
+    * button is clicked and then removes it when the user clicks on the close button within
+    * the modal window.
+    **/
 
+    $(".tutorial-btn").click(function () {
+        $("#tutorial").addClass("d-block");
+        $(".modal-close").click(function () {
+            $(".modal").removeClass("d-block").addClass("d-none");
+        })
+    })
+
+    $(".play-btn").click(function () {
+        $("#levelSelect").addClass("d-block");
+        $(".modal-close").click(function () {
+            $(".modal").removeClass("d-block").addClass("d-none");
+        });
+    })
+
+    /*
+    * The below records the users choice of difficulty level and 
+    * assigns it as the value which will be used to determine 
+    * what difficulty the game will play at. 
+    */
+
+    let difficultyLevel = "";
+
+    $("button.level-btn").click(function (event) {
+        difficultyLevel = $(this).attr('id');
+        console.log(difficultyLevel);
+    });
 
 });
