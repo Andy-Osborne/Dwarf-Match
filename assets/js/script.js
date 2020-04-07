606d6c35f32d50770cc5e04f0748927c6f214a1b
Andy-Osborne <ajaosborne@googlemail.com>
2020-04-07 10:01:08 +0100

    Fix difficultlyLevel and add logic for amount of tiles required for game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
 
     /** 
-    * Modal Button controls works by adding display: block class when the corresponding
-    * button is clicked and then removes it when the user clicks on the close button within
-    * the modal window.
-    **/
+     * Modal Button controls works by adding display: block class when the corresponding
+     * button is clicked and then removes it when the user clicks on the close button within
+     * the modal window.
+     **/
 
     $(".tutorial-btn").click(function () {
         $("#tutorial").addClass("d-block");
@@ -20,17 +20,32 @@ $(document).ready(function () {
         });
     })
 
-    /*
-    * The below records the users choice of difficulty level and 
-    * assigns it as the value which will be used to determine 
-    * what difficulty the game will play at. 
-    */
-
-    let difficultyLevel = "";
-
     $("button.level-btn").click(function (event) {
-        difficultyLevel = $(this).attr('id');
-        console.log(difficultyLevel);
+        $(".landing-page").addClass("d-none");
+        $(".game-play-area").addClass("d-block");
     });
 
 });
+
+
+let gameTiles = 0;
+let difficultyLevel = "";
+
+/**
+ * The below records the users choice of difficulty level and 
+ * assigns it as the value which will be used to determine 
+ * what difficulty the game will play at. Difficuly level is used
+ * to determine the amount of tiles required in the game. 
+ **/
+
+function levelChoice(obj) {
+    difficultyLevel = obj.id;
+
+    if (difficultyLevel == "easy") {
+        gameTiles = 12;
+    } else if (difficultyLevel == "normal") {
+        gameTiles = 18;
+    } else if (difficultyLevel == "hard") {
+        gameTiles = 36;
+    }
+};
