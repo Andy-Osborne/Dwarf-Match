62e002ab09fd154ba67a203a01379739ae5f5830
Andy-Osborne <ajaosborne@googlemail.com>
2020-04-20 20:11:55 +0100

    Add functionality to victory menu buttons

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -2,6 +2,7 @@
 
 document.querySelectorAll(".tutorial-btn").forEach(item => {
     item.addEventListener("click", event => {
+        clickSound();
         document.getElementById("tutorial").classList.add("d-block");
     })
     document.querySelectorAll(".modal-close").forEach(item => {
@@ -9,11 +10,12 @@ document.querySelectorAll(".tutorial-btn").forEach(item => {
             document.getElementById("tutorial").classList.remove("d-block");
         })
     })
-})
+});
 
 // The below is an event listener for the Play Menu modal. 
 
 document.querySelector(".play-btn").addEventListener("click", event => {
+    clickSound();
     document.getElementById("levelSelect").classList.remove("d-none");
     document.getElementById("levelSelect").classList.add("d-block");
 
@@ -22,7 +24,7 @@ document.querySelector(".play-btn").addEventListener("click", event => {
             document.getElementById("levelSelect").classList.remove("d-block");
         })
     })
-})
+});
 
 /** The below is an event listener for when the user clicks on a level button within the
  * Play Menu Modal
@@ -30,6 +32,7 @@ document.querySelector(".play-btn").addEventListener("click", event => {
 
 document.querySelectorAll(".level-btn").forEach(item => {
     item.addEventListener("click", event => {
+        clickSound();
         document.getElementById("landing-page").classList.remove("d-block");
         document.getElementById("landing-page").classList.add("d-none");
         document.getElementById("game-page").classList.remove("d-none");
@@ -38,15 +41,35 @@ document.querySelectorAll(".level-btn").forEach(item => {
         document.getElementById("levelSelect").classList.add("d-none");
         document.getElementById("exit-btn").classList.remove("d-none");
     })
-})
+});
 
 /** The below is an event listener for when the user clicks on the exit button within the
  * game area.
  **/
 
 document.querySelector("#exit-btn").addEventListener("click", event => {
+    clickSound();
+    clearGameArea();
     document.getElementById("game-page").classList.add("d-none");
     document.getElementById("game-page").classList.remove("d-block");
     document.getElementById("landing-page").classList.add("d-block");
     document.getElementById("exit-btn").classList.add("d-none");
-})
+});
+
+document.getElementById("home").addEventListener("click", event => {
+    clickSound();
+    clearGameArea();
+    document.getElementById("victory").classList.add("d-none");
+    document.getElementById("victory").classList.remove("d-block");
+    document.getElementById("game-page").classList.add("d-none");
+    document.getElementById("game-page").classList.remove("d-block");
+    document.getElementById("landing-page").classList.add("d-block");
+    document.getElementById("exit-btn").classList.add("d-none");
+});
+
+document.getElementById("restart-level").addEventListener("click", event => {
+    clickSound();
+    document.getElementById("victory").classList.add("d-none");
+    document.getElementById("victory").classList.remove("d-block");
+    restartLevel();
+});
