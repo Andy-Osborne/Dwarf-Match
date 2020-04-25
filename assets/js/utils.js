011b33f978e0056dc5d72bc07d8eacf335aaccf3
Andy-Osborne <ajaosborne@googlemail.com>
2020-04-25 14:35:32 +0100

    Refactor code and combine both exit events into one event

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -6,11 +6,11 @@ document.querySelectorAll(".tutorial-btn").forEach(item => {
         closeActiveModal();
         document.getElementById("tutorial").classList.add("d-block");
         document.getElementById("tutorial").classList.remove("d-none");
-    });
-    document.querySelectorAll(".modal-close").forEach(item => {
-        item.addEventListener("click", event => {
-            document.getElementById("tutorial").classList.remove("d-block");
-            document.getElementById("tutorial").classList.add("d-none");
+        document.querySelectorAll(".modal-close").forEach(item => {
+            item.addEventListener("click", event => {
+                document.getElementById("tutorial").classList.remove("d-block");
+                document.getElementById("tutorial").classList.add("d-none");
+            });
         });
     });
 });
@@ -21,7 +21,6 @@ document.querySelector(".play-btn").addEventListener("click", event => {
     clickSound();
     document.getElementById("levelSelect").classList.remove("d-none");
     document.getElementById("levelSelect").classList.add("d-block");
-
     document.querySelectorAll(".modal-close").forEach(item => {
         item.addEventListener("click", event => {
             document.getElementById("levelSelect").classList.remove("d-block");
@@ -38,7 +37,6 @@ document.querySelectorAll(".audio-btn").forEach(item => {
         closeActiveModal();
         document.getElementById("soundModal").classList.remove("d-none");
         document.getElementById("soundModal").classList.add("d-block");
-    
         document.querySelectorAll(".modal-close").forEach(item => {
             item.addEventListener("click", event => {
                 document.getElementById("soundModal").classList.remove("d-block");
@@ -77,52 +75,33 @@ document.querySelectorAll(".level-btn").forEach(item => {
     });
 });
 
-/** The below is an event listener for when the user clicks on the exit button within the
- * game area.
- **/
+// The below is an event listener for when the user clicks on the exit/home button.
 
-document.querySelector("#exit-btn").addEventListener("click", event => {
-    clickSound();
-    closeActiveModal();
-    clearGameArea();
-    document.getElementById("victory").classList.add("d-none");
-    document.getElementById("victory").classList.remove("d-block");
-    document.getElementById("game-page").classList.add("d-none");
-    document.getElementById("game-page").classList.remove("d-block");
-    document.getElementById("landing-page").classList.add("d-block");
-    document.getElementById("nav-btns").classList.add("d-none");
-    document.getElementById("title").classList.remove("d-none");
-    document.getElementById("title").classList.add("d-block");
+document.querySelectorAll(".exit-btn").forEach(item => {
+    item.addEventListener("click", event => {
+        clickSound();
+        closeActiveModal();
+        clearGameArea();
+        document.getElementById("victory").classList.add("d-none");
+        document.getElementById("victory").classList.remove("d-block");
+        document.getElementById("game-page").classList.add("d-none");
+        document.getElementById("game-page").classList.remove("d-block");
+        document.getElementById("landing-page").classList.add("d-block");
+        document.getElementById("nav-btns").classList.add("d-none");
+        document.getElementById("title").classList.remove("d-none");
+        document.getElementById("title").classList.add("d-block");
+    });
 });
 
 // The below runs the restart game function
 
-document.getElementById("restart-btn").addEventListener("click", event => {
-    clickSound();
-    closeActiveModal();
-    restartLevel();
-});
-
-// Victory modal listeners.
-
-document.getElementById("home").addEventListener("click", event => {
-    clickSound();
-    clearGameArea();
-    document.getElementById("victory").classList.add("d-none");
-    document.getElementById("victory").classList.remove("d-block");
-    document.getElementById("game-page").classList.add("d-none");
-    document.getElementById("game-page").classList.remove("d-block");
-    document.getElementById("landing-page").classList.add("d-block");
-    document.getElementById("nav-btns").classList.add("d-none");
-    document.getElementById("title").classList.remove("d-none");
-    document.getElementById("title").classList.add("d-block");
-});
-
-document.getElementById("restart-level").addEventListener("click", event => {
-    clickSound();
-    document.getElementById("victory").classList.add("d-none");
-    document.getElementById("victory").classList.remove("d-block");
-    restartLevel();
+document.querySelectorAll(".restart-btn").forEach(item => {
+    item.addEventListener("click", event => {
+        clickSound();
+        closeActiveModal();
+        closeVictoryModal();
+        restartLevel();
+    });
 });
 
 function closeActiveModal() {
@@ -132,9 +111,7 @@ function closeActiveModal() {
     document.getElementById("soundModal").classList.add("d-none");
 }
 
-function disableButtons() {
-    document.getElementById("exit-btn").disabled = true;
-    document.getElementsByName("tutorial-btn").forEach(item => {
-        item.disabled  = true;
-    })
+function closeVictoryModal() {
+    document.getElementById("victory").classList.add("d-none");
+    document.getElementById("victory").classList.remove("d-block");
 }
